1bb65bc391ead89a7a63c77b7fc5e9d58417cd74
Deepa Rajendran <deeparajendran54@gmail.com>
2025-06-23 22:02:43 +0530

    Initial commit

---
+++ src/main/java/com/track_swift/trackswift/TrackswiftApplication.java
@@ -0,0 +1,13 @@
+package com.track_swift.trackswift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackswiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackswiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/track_swift/trackswift/TrackswiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.track_swift.trackswift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackswiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
